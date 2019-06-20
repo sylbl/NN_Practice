85871a8199320e82f800ec76082a6cb42271ad1f
sylbl <shouta1990520@yahoo.co.jp>
2019-06-21 04:57:45 +0900

    sin wave

--- make_data.py
+++ make_data.py
@@ -1,4 +1,32 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import math
 
-point_num = 10000
+
+point_num = 200*4
+init_num = 10000
+
+x = np.arange(0, point_num, 1)
+y5 = np.sin(np.pi/5 * x) / 8
+y25 = np.sin(np.pi/25 * x) / 4
+y75 = np.sin(np.pi/75 * x) / 2
+y200 = np.sin(np.pi/200 * x)
+
+sinW = y5 + y25 + y75 + y200
+
+plt.plot(x, sinW)
+plt.show()
+
+# white noise
+sinN = y5 + y25 + y75 + y200 + (1/8*np.random.randn(point_num))
+
+plt.plot(x, sinN)
+plt.show()
+
+# save csv
+sinData = pd.DataFrame({'sin':sinW,
+                        'noise':sinN})
+sinData.set_index(x, inplace=True)
+sinData.to_csv("./sin_wave.csv")
+
