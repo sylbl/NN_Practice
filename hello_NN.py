2ee9fffbb20de3f9a2a60848c05d585332fb5408
sylbl <shouta1990520@yahoo.co.jp>
2019-06-22 07:19:07 +0900

    check

--- hello_NN.py
+++ hello_NN.py
@@ -1,18 +1,62 @@
 import tensorflow as tf
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 
 # sin wave data読み込み
+sinWaveData = pd.read_csv("./sin_wave.csv")
+sinWaveData = sinWaveData.drop("Unnamed: 0", axis=1)
+print(sinWaveData)
+print(sinWaveData.max())
+
+# 正解データ用意
+sinWaveData['ans'] = sinWaveData['sin'].shift(-1)
+print(sinWaveData)
 
 # data正規化
+scaler = MinMaxScaler()
+scaler.fit(sinWaveData)
+sinWaveDataNorm = scaler.transform(sinWaveData)
 
 # 訓練データ/検証データの分割
+trainData = sinWaveDataNorm[:1000]
+checkData = sinWaveDataNorm[1001:-1]
+
+# 訓練データの生成
+INPUT_DAYS = 5
+iData = []
+aData = []
+for i in range(INPUT_DAYS-1, len(trainData)):
+    inpSet = []
+    for j in range(i-INPUT_DAYS+1, i+1):
+        inpSet.append(trainData[j][0])
+
+    iData.append(inpSet)
+    aData.append([trainData[i][2]])
+iData = np.array(iData)
+aData = np.array(aData)
+
+
+# 検証データの生成
+ciData = []
+caData = []
+for i in range(INPUT_DAYS-1, len(checkData)):
+    inpSet = []
+    for j in range(i-INPUT_DAYS+1, i+1):
+        inpSet.append(checkData[j][0])
+
+    ciData.append(inpSet)
+    caData.append([checkData[i][2]])
+ciData = np.array(ciData)
+caData = np.array(caData)
 
 ## NNの構築
-INPUT_LAYER_NUM = 5
+INPUT_LAYER_NUM = INPUT_DAYS
 HIDDEN_LAYER_NUM = 3
 OUTPUT_LAYER_NUM = 1
 TRAIN_RATE = 0.2
-TRAIN_NUM = 1000
+TRAIN_NUM = 10
 
 #入出力ホルダー
 inpData = tf.placeholder(tf.float32, [None, INPUT_LAYER_NUM])
@@ -37,9 +81,30 @@ train = tf.train.AdamOptimizer(TRAIN_RATE).minimize(loss)
 # 実行
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+
+
     for i in range(TRAIN_NUM):
-        dummy_, loss_, ans_ = sess.run([train, loss, oData],
-                                        feed_dict = {iData: iDatas, oData: oDatas})
-        print(i, loss)
+        dummy_, loss_= sess.run([train, loss],
+                            feed_dict = {inpData: iData, ansData: aData})
+        
+        if i % 100 == 0:
+            print(i, ":", loss_)
+
+    out, loss_ = sess.run([output, loss], feed_dict={inpData: iData, ansData: aData})
+    print(loss_)
+
+    plt.plot(np.reshape(aData, (1,-1))[0])
+    plt.plot(np.reshape(out, (1,-1))[0])
+    plt.show()
+
+    out, loss_ = sess.run([output, loss], feed_dict={inpData: ciData, ansData: caData})
+    print(loss_)
+
+    plt.plot(np.reshape(caData, (1,-1))[0])
+    plt.plot(np.reshape(out, (1,-1))[0])
+    plt.show()
+
+    
+
 
 
