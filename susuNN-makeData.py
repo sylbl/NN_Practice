d9e66634e952dac85d5a9f5aae0e4560f7a20699
sylbl <shouta1990520@yahoo.co.jp>
2019-07-14 17:27:22 +0900

    input

---
+++ susuNN-makeData.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.preprocessing as prep
+#from sklearn import preprocessing
+
+# 生成データ数
+MAKE_DATA_NUM = 200*6
+
+# 合成sin波
+x = np.arange(0, MAKE_DATA_NUM, 1)
+y5 = np.sin(np.pi/5 * x) / 8
+y25 = np.sin(np.pi/25 * x) / 4
+y75 = np.sin(np.pi/75 * x) / 2
+y200 = np.sin(np.pi/200 * x)
+
+sinW = y5 + y25 + y75 + y200
+# 正規化
+sinW = prep.minmax_scale(sinW)
+
+# 合成sin波表示
+plt.plot(x, sinW)
+#plt.savefig("./sinW.png")
+plt.show()
+
+
+# ホワイトノイズの付加
+sinN = y5 + y25 + y75 + y200 + (1/16*np.random.randn(MAKE_DATA_NUM))
+# 正規化
+sinN = prep.minmax_scale(sinN)
+
+# 合成sin波表示
+plt.plot(x, sinN)
+#plt.savefig("./sinN.png")
+plt.show()
+
+
+# save csv
+sinData = pd.DataFrame({'sin':sinW, 'noise':sinN})
+print(sinData)
+sinData.to_csv("./sinwave.csv", index=False)
+
--- susuNN.py
+++ susuNN.py
@@ -6,14 +6,32 @@ import matplotlib.pyplot as plt
 ##--------------------------
 # データの用意
 ##--------------------------
-DATA_FILENAME = 'sinwave.csv'
-
 # データファイル読み込み
+DATA_FILENAME = './sinwave.csv'
+DATA_COLUMN_NAME = 'sin'
+data = pd.read_csv(DATA_FILENAME, usecols=[DATA_COLUMN_NAME])
 
-# 学習データと検証データに分割
 
 # 予測に使用するデータ数
 INPUT_DATA_NUM = 5
+
+# データセットの生成
+inpData = []
+ansData = []
+for i in range(0, len(data) - INPUT_DATA_NUM):
+    print(data[i:i + INPUT_DATA_NUM].values)
+    #print(data[i])
+
+print(inpData)
+
+# 訓練データと検証データに分割
+TRAIN_DATA_NUM = 1000
+trainData = data[:TRAIN_DATA_NUM - 1]
+testData = data[TRAIN_DATA_NUM:]
+
+
+
+# データセットの生成
 
 ##--------------------------
 # NN構築
@@ -44,3 +62,33 @@ loss = tf.reduce_mean(tf.squared_difference(ansData, output))
 # 最適化
 train = tf.train.AdamOptimizer(TRAIN_RATE).minimize(loss)
 
+
+##--------------------------
+# 学習実行
+##--------------------------
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+
+    for i in range(TRAIN_NUM):
+        dummy_, loss_= sess.run([train, loss],
+                            feed_dict = {inpData: iData, ansData: aData})
+        
+        if i % 100 == 0:
+            print(i, ":", loss_)
+
+    out, loss_ = sess.run([output, loss], feed_dict={inpData: iData, ansData: aData})
+    print(loss_)
+
+    plt.plot(np.reshape(aData, (1,-1))[0])
+    plt.plot(np.reshape(out, (1,-1))[0])
+    plt.savefig("./nn(n)-d"+str(INPUT_DAYS)+"t"+str(TRAIN_NUM)+"r"+str(TRAIN_RATE)+"gaku.png")
+    plt.show()
+
+    out, loss_ = sess.run([output, loss], feed_dict={inpData: ciData, ansData: caData})
+    print(loss_)
+
+    plt.plot(np.reshape(caData, (1,-1))[0])
+    plt.plot(np.reshape(out, (1,-1))[0])
+    plt.savefig("./nn(n)-d"+str(INPUT_DAYS)+"t"+str(TRAIN_NUM)+"r"+str(TRAIN_RATE)+"chk.png")
+    plt.show()
